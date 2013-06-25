81ea5f1fb8caa986f169ded9ba80fa5a126a06a3
Yuta Hayakawa <hayazou@gmail.com>
2013-06-25 15:59:42 +0900

    Move some classes for autoloader

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * コントローラ起動ルーチン
+ * Risoluto起動ルーチン
  *
- * コントローラのクラスインスタンスを作成し、実行する
+ * Risolutoコアクラスインスタンスを作成し、実行する
  *
  * @package   risoluto
  * @author    Risoluto Developers
@@ -10,74 +10,64 @@
  * @copyright (C) 2008-2013 Risoluto Developers / All Rights Reserved.
  */
 
-  //------------------------------------------------------//
-  // 動作環境チェック
-  //------------------------------------------------------//
-  // PHPバージョンが5.0.0以前であれば強制終了
-  if ( version_compare( PHP_VERSION, '5.0.0', '<' ) )
-  {
-    exit( 'Risoluto requires PHP 5.0.0 or later...' );
-  } // end of if
+//------------------------------------------------------//
+// 名前空間の定義
+//------------------------------------------------------//
+namespace Risoluto;
 
-  // php.iniの設定値が動作上支障を及ぼす可能性がある場合強制終了
-  if (
-          ini_get( 'magic_quotes_gpc' )
-       or ini_get( 'magic_quotes_runtime' )
-       or ini_get( 'magic_quotes_sybase' )
-       or ini_get( 'register_globals' )
-       or ini_get( 'register_long_arrays' )
-       or ini_get( 'safe_mode' )
-     )
-  {
-    exit( 'magic_quotes_gpc/magic_quotes_runtime/magic_quotes_sybase/register_globals/register_long_arrays/safe_mode are MUST be off.' );
-  } // end of if
+//------------------------------------------------------//
+// 動作環境チェック
+//------------------------------------------------------//
+// PHPバージョンが指定された以前のものであれば強制終了
+if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    exit('Risoluto requires PHP 5.5.0 or later...');
+}
 
-  // OPTIONAL: URLの正規化
-//  if ( isset( $_SERVER[ 'SERVER_NAME' ] ) and ! preg_match( '/^www.*/i', $_SERVER[ 'SERVER_NAME' ] ) )
-//  {
-//    if ( isset( $_SERVER[ 'HTTPS' ] ) and !empty( $_SERVER[ 'HTTPS' ] ) )
-//    {
-//      header( 'Location: https://www.' . $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'REQUEST_URI' ], true, 301 );
-//    } // end of if
-//    else
-//    {
-//      header( 'Location: http://www.' . $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'REQUEST_URI' ], true, 301 );
-//    } // end of else
-//    exit;
-//  } // end of if
+// // OPTIONAL: URLの正規化
+// if (isset($_SERVER['SERVER_NAME']) and !preg_match('/^www.*/i', $_SERVER['SERVER_NAME'])) {
+//     header('Location: ' . ((isset($_SERVER['HTTPS']) and !empty($_SERVER['HTTPS'])) ? 'https' : 'http')
+//                         . '://www.' $_SERVER[ 'SERVER_NAME' ] . $_SERVER[ 'REQUEST_URI' ], true, 301);
+//     exit;
+// }
 
-  //------------------------------------------------------//
-  // 定数定義
-  //------------------------------------------------------//
-  define( 'RISOLUTODIR',       dirname( dirname ( __FILE__ ) )     );
-  define( 'RISOLUTO_DOCROOT',  dirname ( __FILE__ )                );
-  define( 'RISOLUTO_SYSROOT',  RISOLUTODIR . '/risoluto/'          );
-  define( 'RISOLUTO_BATCH',    RISOLUTODIR . '/risoluto/batch/'    );
-  define( 'RISOLUTO_CACHE',    RISOLUTODIR . '/risoluto/cache/'    );
-  define( 'RISOLUTO_CONF',     RISOLUTODIR . '/risoluto/conf/'     );
-  define( 'RISOLUTO_DATA',     RISOLUTODIR . '/risoluto/data/'     );
-  define( 'RISOLUTO_EXTLIB',   RISOLUTODIR . '/risoluto/extlib/'   );
-  define( 'RISOLUTO_FUNC',     RISOLUTODIR . '/risoluto/func/'     );
-  define( 'RISOLUTO_LOGS',     RISOLUTODIR . '/risoluto/logs/'     );
-  define( 'RISOLUTO_SESS',     RISOLUTODIR . '/risoluto/sess/'     );
-  define( 'RISOLUTO_UPLOAD',   RISOLUTODIR . '/risoluto/upload/'   );
-  define( 'RISOLUTO_USERLAND', RISOLUTODIR . '/risoluto/userland/' );
+//------------------------------------------------------//
+// 定数定義
+//------------------------------------------------------//
+define('RISOLUTODIR', dirname(dirname(__FILE__)));
 
-  //------------------------------------------------------//
-  // インクルードパスの変更
-  //------------------------------------------------------//
-  set_include_path(                 RISOLUTO_EXTLIB . 'PEAR' 
-                 . PATH_SEPARATOR . RISOLUTO_EXTLIB . 'Smarty/libs'
-                 . PATH_SEPARATOR . get_include_path() );
+define('RISOLUTO_DOCROOT', dirname(__FILE__));
+define('RISOLUTO_SYSROOT', RISOLUTODIR . '/risoluto/');
 
-  //------------------------------------------------------//
-  // コントローラクラスの読み込み
-  //------------------------------------------------------//
-  require_once( RISOLUTO_FUNC . 'risoluto_core.php' );
+define('RISOLUTO_APPS', RISOLUTODIR . '/risoluto/apps/');
+define('RISOLUTO_CLI',  RISOLUTODIR . '/risoluto/cli/');
+define('RISOLUTO_CONF', RISOLUTODIR . '/risoluto/conf/');
+define('RISOLUTO_DATA', RISOLUTODIR . '/risoluto/data/');
+define('RISOLUTO_LIB',  RISOLUTODIR . '/risoluto/lib/');
 
-  //------------------------------------------------------//
-  // コントローラクラスインスタンスの生成と実行
-  //------------------------------------------------------//
-  $risoluto_instance = RisolutoCore::singleton();
-  $risoluto_instance->run();
-?>
+define('RISOLUTO_CACHE',  RISOLUTO_DATA . 'cache/');
+define('RISOLUTO_LOGS',   RISOLUTO_DATA . 'logs/');
+define('RISOLUTO_SESS',   RISOLUTO_DATA . 'sess/');
+define('RISOLUTO_UPLOAD', RISOLUTO_DATA . 'upload/');
+
+define('RISOLUTO_LIB_USR',    RISOLUTO_LIB . 'usr/');
+define('RISOLUTO_LIB_3RD',    RISOLUTO_LIB . '3rd/');
+define('RISOLUTO_LIB_VENDOR', RISOLUTO_LIB . 'vendor/');
+
+//------------------------------------------------------//
+// インクルードパスの変更
+//------------------------------------------------------//
+set_include_path(RISOLUTO_LIB_USR    . PATH_SEPARATOR
+               . RISOLUTO_LIB_3RD    . PATH_SEPARATOR
+               . RISOLUTO_LIB_VENDOR . PATH_SEPARATOR
+               . get_include_path());
+
+//------------------------------------------------------//
+// Risolutoコアクラスの読み込み
+//------------------------------------------------------//
+require_once(RISOLUTO_LIB_VENDOR . 'RisolutoCore.php');
+
+//------------------------------------------------------//
+// Risolutoコアクラスインスタンスの生成と実行
+//------------------------------------------------------//
+$risoluto_instance = new RisolutoCore;
+$risoluto_instance->run();
