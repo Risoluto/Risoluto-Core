2c8ef024a17d00e4ca459db2b9613444757fac02
Yuta Hayakawa <haya@risoluto.org>
2014-09-04 21:57:02 +0900

    Re-format code

--- risoluto/lib/phpunit_bootstrap.php
+++ risoluto/lib/phpunit_bootstrap.php
@@ -26,44 +26,44 @@ if (version_compare(PHP_VERSION, '5.5.0', '<')) {
 //------------------------------------------------------//
 // 定数定義
 //------------------------------------------------------//
-if(!defined('RISOLUTODIR')){
-define('RISOLUTODIR', dirname(dirname(dirname(__FILE__))));
+if (!defined('RISOLUTODIR')) {
+    define('RISOLUTODIR', dirname(dirname(dirname(__FILE__))));
 }
-if(!defined('RISOLUTO_DOCROOT')){
-define('RISOLUTO_DOCROOT', RISOLUTODIR . 'public_html');
+if (!defined('RISOLUTO_DOCROOT')) {
+    define('RISOLUTO_DOCROOT', RISOLUTODIR . 'public_html');
 }
-if(!defined('RISOLUTO_SYSROOT')){
-define('RISOLUTO_SYSROOT', RISOLUTODIR . '/risoluto/');
+if (!defined('RISOLUTO_SYSROOT')) {
+    define('RISOLUTO_SYSROOT', RISOLUTODIR . '/risoluto/');
 }
-if(!defined('RISOLUTO_APPS')){
-define('RISOLUTO_APPS', RISOLUTO_SYSROOT . 'apps/');
+if (!defined('RISOLUTO_APPS')) {
+    define('RISOLUTO_APPS', RISOLUTO_SYSROOT . 'apps/');
 }
-if(!defined('RISOLUTO_CLI')){
-define('RISOLUTO_CLI', RISOLUTO_SYSROOT . 'cli/');
+if (!defined('RISOLUTO_CLI')) {
+    define('RISOLUTO_CLI', RISOLUTO_SYSROOT . 'cli/');
 }
-if(!defined('RISOLUTO_CONF')){
-define('RISOLUTO_CONF', RISOLUTO_SYSROOT . 'conf/');
+if (!defined('RISOLUTO_CONF')) {
+    define('RISOLUTO_CONF', RISOLUTO_SYSROOT . 'conf/');
 }
-if(!defined('RISOLUTO_DATA')){
-define('RISOLUTO_DATA', RISOLUTO_SYSROOT . 'data/');
+if (!defined('RISOLUTO_DATA')) {
+    define('RISOLUTO_DATA', RISOLUTO_SYSROOT . 'data/');
 }
-if(!defined('RISOLUTO_LIB')){
-define('RISOLUTO_LIB', RISOLUTO_SYSROOT . 'lib/');
+if (!defined('RISOLUTO_LIB')) {
+    define('RISOLUTO_LIB', RISOLUTO_SYSROOT . 'lib/');
 }
-if(!defined('RISOLUTO_CACHE')){
-define('RISOLUTO_CACHE', RISOLUTO_DATA . 'cache/');
+if (!defined('RISOLUTO_CACHE')) {
+    define('RISOLUTO_CACHE', RISOLUTO_DATA . 'cache/');
 }
-if(!defined('RISOLUTO_LOGS')){
-define('RISOLUTO_LOGS', RISOLUTO_DATA . 'logs/');
+if (!defined('RISOLUTO_LOGS')) {
+    define('RISOLUTO_LOGS', RISOLUTO_DATA . 'logs/');
 }
-if(!defined('RISOLUTO_SESS')){
-define('RISOLUTO_SESS', RISOLUTO_DATA . 'sess/');
+if (!defined('RISOLUTO_SESS')) {
+    define('RISOLUTO_SESS', RISOLUTO_DATA . 'sess/');
 }
-if(!defined('RISOLUTO_UPLOAD')){
-define('RISOLUTO_UPLOAD', RISOLUTO_DATA . 'upload/');
+if (!defined('RISOLUTO_UPLOAD')) {
+    define('RISOLUTO_UPLOAD', RISOLUTO_DATA . 'upload/');
 }
-if(!defined('RISOLUTO_LIB_VENDOR')){
-define('RISOLUTO_LIB_VENDOR', RISOLUTO_LIB . 'vendor/');
+if (!defined('RISOLUTO_LIB_VENDOR')) {
+    define('RISOLUTO_LIB_VENDOR', RISOLUTO_LIB . 'vendor/');
 }
 //------------------------------------------------------//
 // インクルードパスの変更
